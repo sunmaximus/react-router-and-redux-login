1407973416b735c97f5b0f9adb0e8dc5c90c21dd
son_nguyen@student.uml.edu <son_nguyen@student.uml.edu>
2018-10-26 14:17:56 -0400

    adding Admin Page

---
+++ src/Home/components/AdminComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Card, Image, Button } from 'semantic-ui-react';
+
+import '../styles/home.scss'
+
+class UserComponent extends Component {
+  state = {
+
+  }
+
+  render() {
+
+    return (
+      <div className='home__container'>
+        admin
+      </div>
+    );
+  }
+}
+
+export default UserComponent;
--- src/Home/components/HomeComponent.js
+++ src/Home/components/HomeComponent.js
@@ -1,37 +1,18 @@
 import React, { Component } from 'react';
-import { Card, Icon, Image, Button } from 'semantic-ui-react';
+import { Card, Image, Button } from 'semantic-ui-react';
+
+import UserComponent from './UserComponent';
+import AdminComponent from './AdminComponent';
 
 import '../styles/home.scss'
-import { Link } from "react-router-dom";
-
+// authenticationLevel
 class HomeComponent extends Component {
-  state = {
-    firstName: this.props.user.firstName,
-    lastName: this.props.user.lastName,
-    birthDay: this.props.user.birthDay,
-    descriptions: this.props.user.descriptions,
-  }
-
   render() {
-    const { firstName, lastName, birthDay, descriptions } = this.state; 
+    if (this.props.user.authenticationLevel === 'user') {
+      return <UserComponent user={this.props.user} />
+    }
 
-    return (
-      <div className='home__container'>
-        <Card>
-          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
-          <Card.Content>
-            <Card.Header>{`${firstName} ${lastName}`}</Card.Header>
-            <Card.Meta>
-              <span className='date'>{birthDay}</span>
-            </Card.Meta>
-            <Card.Description>{descriptions}</Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <Button>Edit</Button>
-          </Card.Content>
-        </Card>
-      </div>
-    );
+    return <AdminComponent user={this.props.user} />
   }
 }
 
