ffcd9b93af1af3a08e3e81f8532109bfe149698a
son_nguyen@student.uml.edu <son_nguyen@student.uml.edu>
2018-10-26 00:33:40 -0400

    added a user card and description when a normal user login

--- src/Home/components/HomeComponent.js
+++ src/Home/components/HomeComponent.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-import '../styles/home.scss'
+import { Card, Icon, Image, Button } from 'semantic-ui-react';
 
+import '../styles/home.scss'
 import { Link } from "react-router-dom";
 
 class HomeComponent extends Component {
+  state = {
+    firstName: this.props.user.firstName,
+    lastName: this.props.user.lastName,
+    birthDay: this.props.user.birthDay,
+    descriptions: this.props.user.descriptions,
+  }
 
   render() {
+    const { firstName, lastName, birthDay, descriptions } = this.state; 
+
     return (
       <div className='home__container'>
-        <h3>Welcome {this.props.user.firstName}</h3>
+        <Card>
+          <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
+          <Card.Content>
+            <Card.Header>{`${firstName} ${lastName}`}</Card.Header>
+            <Card.Meta>
+              <span className='date'>{birthDay}</span>
+            </Card.Meta>
+            <Card.Description>{descriptions}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Button>Edit</Button>
+          </Card.Content>
+        </Card>
       </div>
     );
   }
--- src/auth/fakeAuth.js
+++ src/auth/fakeAuth.js
@@ -3,6 +3,7 @@ const regularUser = {
   lastName: 'Hank',
   authenticationLevel: 'user',
   birthDay: 'July 9, 1956',
+  descriptions: 'The Hulk is a fictional superhero appearing in American comic books published by Marvel Comics. Created by writer Stan Lee and artist Jack Kirby, the character first appeared in the debut issue of The Incredible Hulk.'
 }
 
 const adminUser = {
@@ -17,7 +18,7 @@ const fakeAuth = {
   authenticate(cb, userInfo) {
     const user = userInfo.username === 'user' ? regularUser : adminUser;
     this.isAuthenticated = true;
-    setTimeout(() => cb(user), 2000); // fake async
+    setTimeout(() => cb(user), 100); // fake async
   },
   signout(cb) {
     this.isAuthenticated = false;
